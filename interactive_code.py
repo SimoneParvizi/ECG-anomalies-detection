41b068e7f121009d03aaaac28a2dc0821d9a7b91
Simone <simone.parvizi@outlook.it>
2022-08-08 16:51:16 +0200

    Update interactive_code.py
    Dataframe finalized

--- interactive_code.py
+++ interactive_code.py
@@ -1,4 +1,5 @@
 #%%
+from email import header
 import torch
 import copy
 import numpy as np
@@ -14,8 +15,26 @@ from scipy.io.arff import loadarff
 
 
 
-rcParams['figure.figsize']
+rcParams['figure.figsize'] = 12.0, 9.0
 
 # %%
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+#%%
+df_TRAIN = pd.read_csv(r"C:\Users\simon\Desktop\Projects DL\ECG anomalies detection\ECG5000\ECG5000_TRAIN.txt", header = None, delim_whitespace=True)
+df_TRAIN.head()
+
+
+df_TEST = pd.read_csv(r"C:\Users\simon\Desktop\Projects DL\ECG anomalies detection\ECG5000\ECG5000_TEST.txt", header = None, delim_whitespace=True)
+df_TEST.head()
+df_TEST.shape
+# %% concatenating the whole dataframe and checking it has the right lenght (5000x141)
+final_df = pd.concat([df_TRAIN, df_TEST])
+final_df.shape
+
+#Renaming the target column
+mapping = {final_df.columns[0]:'target (1,2,3,4,5)'}
+final_df = final_df.rename(columns=mapping)
+final_df.head()
 # %%
