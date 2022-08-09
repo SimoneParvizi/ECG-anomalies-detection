bd00e4c1cd0e5b89f3c249112b005de223b4df76
Simone <simone.parvizi@outlook.it>
2022-08-09 11:58:20 +0200

    Update interactive_code.py

--- interactive_code.py
+++ interactive_code.py
@@ -1,5 +1,6 @@
 #%%
 from email import header
+from sre_parse import fix_flags
 import torch
 import copy
 import numpy as np
@@ -29,12 +30,62 @@ df_TRAIN.head()
 df_TEST = pd.read_csv(r"C:\Users\simon\Desktop\Projects DL\ECG anomalies detection\ECG5000\ECG5000_TEST.txt", header = None, delim_whitespace=True)
 df_TEST.head()
 df_TEST.shape
+
+
 # %% concatenating the whole dataframe and checking it has the right lenght (5000x141)
 final_df = pd.concat([df_TRAIN, df_TEST])
 final_df.shape
 
 #Renaming the target column
-mapping = {final_df.columns[0]:'target (1,2,3,4,5)'}
+mapping = {final_df.columns[0]:'targets'}
 final_df = final_df.rename(columns=mapping)
 final_df.head()
+
+
+# %% Shuffling data
+final_df = final_df.sample(frac=1.0)
+final_df.head()
+
+#%% Data Exploration
+
+final_df.targets.value_counts().plot(kind='barh')
+final_df.targets.value_counts()
+#they are imbalanced indeed. WHAT ARE THE LIMITS OF THIS IMBALANCE? WHEN IS IT ACCEPTABLE?
+
+
+
+# %% Let's take a look at the different classes
+def plot_time_series_class(data, class_name, ax, n_steps=10):
+    time_series_df = pd.DataFrame(data)
+
+    smooth_path = time_series_df.rolling(n_steps).mean()
+    path_deviation = 2 * time_series_df.rolling(n_steps).std()
+
+    under_line = (smooth_path - path_deviation)[0]
+    over_line = (smooth_path + path_deviation)[0]
+
+    ax.plot(smooth_path, linewidth=2)
+    ax.fill_between(path_deviation.index, under_line, over_line, alpha=0.125)
+
+    ax.set_title(class_name)
+
+#%%
+classes = final_df.targets.unique()
+class_name = ['Normal', 'R on T', 'PVC', 'SP', 'UB']
+
+fig, axs = plt.subplots(
+    nrows=1,
+    ncols=len(classes),
+    sharey=True,
+    figsize=(14,2)
+)
+
+
+for i, cls in enumerate(classes):
+    ax = axs[i]
+    data = final_df[final_df.targets == cls].drop(labels='targets', axis=1).mean(axis=0).to_numpy()
+    plot_time_series_class(data, class_name[i], ax, n_steps=10)
+
+
+
 # %%
