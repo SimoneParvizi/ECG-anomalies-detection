0c294c494fca991cab4cfe12e76ba8ea35490479
Simone <simone.parvizi@outlook.it>
2023-03-31 09:16:06 +0200

    Update interactive_code.py

--- interactive_code.py
+++ interactive_code.py
@@ -47,7 +47,9 @@ final_df = final_df.sample(frac=1.0)
 final_df.head()
 
 #%% Data Exploration
+type(final_df)
 
+#%%
 final_df.targets.value_counts().plot(kind='barh')
 final_df.targets.value_counts()
 #they are imbalanced indeed. WHAT ARE THE LIMITS OF THIS IMBALANCE? WHEN IS IT ACCEPTABLE?
@@ -146,33 +148,37 @@ class Encorder(nn.Module):
     
 
 class Dencorder(nn.Module):
-    def __init__(self, seq_len, n_features, embedding_dim=64):
+    def __init__(self, seq_len, input_dim=64, output_dim=1):
         super(Dencorder, self).__init__()
-        self.seq_len, self.n_features = seq_len, n_features
-        self.embedding_dim = embedding_dim
-        self.hidden_dim = 2 * embedding_dim
+        self.seq_len, self.input_dim = seq_len, input_dim
+        self.hidden_dim, self.output_dim = 2 * input_dim, output_dim
 
         self.rnn1 = nn.LSTM(
-            input_size=n_features,
-            hidden_size=self.hidden_dim,
+            input_size=input_dim,
+            hidden_size=input_dim,
             num_layers=1
             batch_first=True
         ) 
 
         self.rnn2 = nn.LSTM(
-            input_size=self.hidden_dim,
-            hidden_size=embedding_dim,
+            input_size=input_dim,
+            hidden_size=self.hidden_dim,
             num_layers=1
             batch_first=True
         )
 
+
+        self.dense_layer = nn.Linear(self.hidden_dim, output_dim)
+
     def forward(self,x):
-        x = x.reshape((1, self.seq_len, self.n_features))
+        x = x.repeat(self.seq_len, 1)
+        x = x.reshape((1, self.seq_len, self.input_dim))
 
         x, (hidden_n, cell_n) = self.rnn1(x)
         x, (hidden_n, cell_n) = self.rnn2(x)
+        x = x.reshape((self.seq_len, self.hidden_dim))
 
-        return hidden_n.reshape((1, self.embedding_dim))
+        return self.dense_layer(x)
 
 
 
@@ -190,3 +196,57 @@ class RAE(nn.Module):
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
+
+        return x
+
+
+
+model = RAE(seq_len, n_features, embedding_dim=128)
+
+
+#%% Training
+
+def train_model(model, train_dataset, val_dataset, n_epochs):
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.L1loss(reduction='sum').to(device)
+    history = dict(train=[], val=[])
+
+    for epoch in range(1, n_epoch + 1):
+        model = model.train()
+        train_losses = []
+
+        for seq_true in train_dataset:
+            opitmizer.zero_grad()
+            seq_pred = model(seq_true)
+
+            loss = criterion(seq_pred, seq_true)
+            loss.backward()
+            optimizer.step()
+
+            train_losses.append(loss.item())
+
+
+        val_losses = []
+        model = model.eval()
+        with torch.no_grad():
+            for seq_true in val_dataset:
+                seq_pred = model(seq_true)
+
+                loss = criterion(seq_pred, seq_true)
+                val_losses.append(loss.item())
+
+        
+        train_loss = np.mean(train_losses)
+        val_loss = np.mean(val_losses)
+        history['train'].append(train_loss)
+        history['val'].append(val_loss)
+
+        print(f'Epoch {epoch}: train loss {train_loss}, val loss {val_loss}')
+
+    
+    return model.eval(), history
+
+
+
+
+# %%
